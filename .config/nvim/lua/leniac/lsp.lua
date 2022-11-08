135cc91e4b121ae9244cf0300d5e8053ff6452a8
Lenilson Nascimento <lenilson.100@gmail.com>
2022-11-09 00:34:51 +0100

    remove unused languages from lsp

--- .config/nvim/lua/leniac/lsp.lua
+++ .config/nvim/lua/leniac/lsp.lua
@@ -7,7 +7,7 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require('lspconfig')
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'eslint' }
+local servers = { 'gopls', 'eslint' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
