6dd0a5b94c1931bd00a6a6b41a815556f2750c87
Lenilson Nascimento <leni@a52.eu>
2023-07-28 15:20:15 +0200

    add electron flags for hyprland

--- scripts/hyprland_desktop.sh
+++ scripts/hyprland_desktop.sh
@@ -6,6 +6,8 @@ ln -sf ${DOTFILESDIR}/hyprland/hypr ${HOME}/.config/hypr
 
 ln -sf ${DOTFILESDIR}/hyprland/hypr/hyprland_desktop.conf ${HOME}/.config/hypr/hyprland.conf
 
+ln -sf ${DOTFILESDIR}/hyprland/electron-flags.conf ${HOME}/.config/electron-flags.conf
+
 ln -sf ${DOTFILESDIR}/hyprland/kitty ${HOME}/.config/kitty
 
 ln -sf ${DOTFILESDIR}/hyprland/.zprofile ${HOME}/.zprofile
