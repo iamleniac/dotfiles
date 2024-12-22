231d5353288e927064fb1bddab97a037f5091ecc
Lenilson Nascimento <lenilson.nascimentolima@gi-de.com>
2024-12-22 05:00:09 +0100

    remove neovim for submodule, remove hyprland

--- .config/nvim/lua/leniac/plugins/others.lua
+++
@@ -1,4 +0,0 @@
-return {
-	'tpope/vim-surround',
-	{ 'numToStr/Comment.nvim', opts = {} }
-}
--- .config/nvim/lua/leniac/plugins/peek.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'toppair/peek.nvim',
-    build = 'deno task --quiet build:fast',
-    config = function()
-        require('peek').setup({
-            app = 'browser',
-        });
-    end
-}
