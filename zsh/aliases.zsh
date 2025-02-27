eba4d523fde96999566d1393e420d763465631f7
Lenilson Nascimento <lenilson.nascimentolima@gi-de.com>
2025-02-27 18:43:40 +0100

    update nvim, add some git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,34 @@
 alias vim=nvim
 alias v=nvim
 
+# helper functions
+yellow="\e[33m"
+reset="\e[0m"
+runcmd() {
+  echo -e "${yellow}${@}${reset}"
+  $@
+}
+
+# Git
+gprune() {
+  runcmd git fetch origin --prune
+  runcmd git branch -vv | grep 'gone]' | awk '{print $1}' | xargs git branch -D
+}
+
+gdef() {
+  runcmd git remote show origin | grep "HEAD branch" | cut -d ":" -f 2 | xargs
+}
+
+gum() {
+  runcmd gprune
+  runcmd git checkout "$(gdef)"
+  runcmd git pull origin "$(gdef)"
+}
+
+gundo() {
+  runcmd git reset --soft HEAD~1
+}
+
 # LS
 alias ls='ls -F --color=always --group-directories-first'
 alias ll='ls -la'
